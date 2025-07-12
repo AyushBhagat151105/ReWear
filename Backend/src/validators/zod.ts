87347a317dd94b66136cec8130f227c919af4326
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-07-12 12:33:28 +0530

    feat: add item creation functionality with image upload and validation

--- Backend/src/validators/zod.ts
+++ Backend/src/validators/zod.ts
@@ -3,13 +3,14 @@ import { z } from "zod";
 const SIZE = ["xs", "s", "m", "l", "xla"] as const;
 const CONDITION = ["NEW", "USED", "GOOD", "FAIR", "POOR"] as const;
 
-export const registerSchema = z.object({
-  title: z.string().min(2, "Name is Required"),
-  description: z.string().min(5, "Description Required"),
-  category: z.object({
-    name: z.string().min(2, "Category is Required"),
+export const itemCreateSchema = z.object({
+  title: z.string().min(2, "Title is required"),
+  description: z.string().min(5, "Description is required"),
+  category: z.string().min(2, "Category is required"),
+  size: z.enum(SIZE, { errorMap: () => ({ message: "Invalid size" }) }),
+  point: z.number().min(1, "Point must be at least 1"),
+  condition: z.enum(CONDITION, {
+    errorMap: () => ({ message: "Invalid condition" }),
   }),
-  size: z.enum(SIZE),
-  point: z.number().min(1, "Point is Required"),
-  condition: z.enum(CONDITION),
+  userid: z.string().uuid("Invalid user ID"),
 });
